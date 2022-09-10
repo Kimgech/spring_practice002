73f917e3fdb45cf98c071afadc40afc2d2ffddb4
kimgech <kimgech309@gmail.com>
2022-09-10 10:04:14 +0700

    Initial commit

---
+++ src/main/kotlin/com/example/spring_practice002/SpringPractice002Application.kt
@@ -0,0 +1,11 @@
+package com.example.spring_practice002
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringPractice002Application
+
+fun main(args: Array<String>) {
+    runApplication<SpringPractice002Application>(*args)
+}
---
+++ src/test/kotlin/com/example/spring_practice002/SpringPractice002ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_practice002
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringPractice002ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
